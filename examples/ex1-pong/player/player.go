4faa2c0bb405ce04f18c1edea1e83c443d524042
Richard Hawkins <hurricanerix@gmail.com>
2016-02-24 17:07:58 -0600

    Fix events

--- examples/ex1-pong/player/player.go
+++ examples/ex1-pong/player/player.go
@@ -22,8 +22,8 @@ import (
 	"github.com/go-gl/mathgl/mgl32"
 	"github.com/hurricanerix/shade/entity"
 	"github.com/hurricanerix/shade/events"
-	"github.com/hurricanerix/shade/sprite"
 	"github.com/hurricanerix/shade/shapes"
+	"github.com/hurricanerix/shade/sprite"
 )
 
 const NumToWin = 5
@@ -32,17 +32,17 @@ const BottomY = 75
 
 // Player state
 type Player struct {
-	pos    mgl32.Vec3
-	Score  int
-	Sprite *sprite.Context
-	Shape    shapes.Shape
-	PlayerNum	int  // player 1 or player 2
+	pos        mgl32.Vec3
+	Score      int
+	Sprite     *sprite.Context
+	Shape      shapes.Shape
+	PlayerNum  int // player 1 or player 2
 	paddleSize int
-	upKey  bool
-	downKey bool
-	keyUp	glfw.Key
-	keyDown	glfw.Key
-	velocity	int
+	upKey      bool
+	downKey    bool
+	keyUp      glfw.Key
+	keyDown    glfw.Key
+	velocity   int
 }
 
 func New(playerNum int, x, y float32, s *sprite.Context) *Player {
@@ -50,16 +50,16 @@ func New(playerNum int, x, y float32, s *sprite.Context) *Player {
 	fmt.Println(s.Width)
 	fmt.Println(s.Height)
 	p := Player{
-		pos:    mgl32.Vec3{x, y, 0.0},
-		Sprite: s,
-		Shape: *shapes.NewRect(0, 10, 0, float32(s.Height) * 2),
-		PlayerNum: playerNum,
+		pos:        mgl32.Vec3{x, y, 0.0},
+		Sprite:     s,
+		Shape:      *shapes.NewRect(0, 10, 0, float32(s.Height)*2),
+		PlayerNum:  playerNum,
 		paddleSize: 8,
-		velocity: 7,
+		velocity:   7,
 	}
 
 	// assign keys to player
-	if p.PlayerNum == 1 { 
+	if p.PlayerNum == 1 {
 		p.keyUp = glfw.KeyQ
 		p.keyDown = glfw.KeyA
 	} else {
@@ -86,19 +86,19 @@ func (p Player) Bounds() shapes.Shape {
 func (p *Player) Handle(event events.Event) {
 	// TODO: move this to SDK to handle things like holding Left & Right at the same time correctly
 
-	if (event.Action == glfw.Press || event.Action == glfw.Repeat) && event.Key == p.keyUp {
+	if (event.Type == events.KeyDown || event.Type == events.KeyRepeat) && event.Key == p.keyUp {
 		p.upKey = true
 	}
-	if (event.Action == glfw.Press || event.Action == glfw.Repeat) && event.Key == p.keyDown {
+	if (event.Type == events.KeyDown || event.Type == events.KeyRepeat) && event.Key == p.keyDown {
 		p.downKey = true
 	}
 
-	if event.Action == glfw.Release && event.Key == p.keyUp {
+	if event.Type == events.KeyUp && event.Key == p.keyUp {
 		p.upKey = false
 	}
-	if event.Action == glfw.Release && event.Key == p.keyDown {
+	if event.Type == events.KeyUp && event.Key == p.keyDown {
 		p.downKey = false
-	}	
+	}
 }
 
 // Update(dt?, group?)
@@ -119,14 +119,14 @@ func (p Player) Draw() {
 	// DrawFrame(frame to render, position, effect); postion 0,0 is bottom left corner of screen
 	// draw top of paddle
 	p.Sprite.DrawFrame(mgl32.Vec2{0, 0}, mgl32.Vec3{posX, posY, 0}, nil)
-	
+
 	// draw middle part(s) of paddle; paddle shrink by 1 on every win
-	for i:=0; i < NumToWin + 1 - p.Score; i++ {
+	for i := 0; i < NumToWin+1-p.Score; i++ {
 		// position of paddle middle parts are offset by player posY minus (paddleSize * i + 1, i.e. index of loop + 1)
-		midPosY := posY - float32(p.paddleSize * (i + 1))
+		midPosY := posY - float32(p.paddleSize*(i+1))
 		p.Sprite.DrawFrame(mgl32.Vec2{0, 1}, mgl32.Vec3{posX, midPosY, 0}, nil)
 	}
 
 	// draw bottom of paddle
-	p.Sprite.DrawFrame(mgl32.Vec2{0, 2}, mgl32.Vec3{posX, posY - float32(p.paddleSize * (NumToWin + 2)) , 0}, nil)
+	p.Sprite.DrawFrame(mgl32.Vec2{0, 2}, mgl32.Vec3{posX, posY - float32(p.paddleSize*(NumToWin+2)), 0}, nil)
 }
